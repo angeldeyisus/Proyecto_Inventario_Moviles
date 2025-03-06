be7ea340cf70613873b9b4d82044b0c8d41ed7ab
Angel Santos <angeldeyisus@users.noreply.github.com>
2025-03-05 20:04:29 -0700

    Se agregó la pantalla inicial y login

--- app/src/main/java/santos/angel/proyectofinalmoviles/login.kt
+++ app/src/main/java/santos/angel/proyectofinalmoviles/login.kt
@@ -1,21 +1,19 @@
 package santos.angel.proyectofinalmoviles
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val buttonStart: Button = findViewById(R.id.button_start)
+        setContentView(R.layout.activity_login)
+        val textViewNoAccount: TextView = findViewById(R.id.textViewNoAccount)
 
-        buttonStart.setOnClickListener {
-
-        }
     }
 }
